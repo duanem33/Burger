25bf9a526dc6544fd46fe4d08bc4521d655b2775
Duane Maldonado <duanem33@gmail.com>
2020-09-28 18:58:11 -0400

    Added DB to Burger Repo

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+---seeds Burgers into Database
+
+INSERT INTO burgers_db.burgers(burger_name, devoured, date)
+VALUES("Big Mac", false, "2019-07-01 04:20:00"),
+ ("Cheeseburger", false, "2019-07-01 04:20:01"),
+  ("Double Cheeseburger", false, "2019-07-01 04:20:02"),
+  ("McDoubles", false, "2019-07-01 16:20:00"),
+  ("Whooper", false, "2019-07-01 16:20:01"),
+  ("Quarter Pounder", false, "2019-07-01 04:20:20"),
+  ("Double Double", true, "2016-07-01 16:20:16");
