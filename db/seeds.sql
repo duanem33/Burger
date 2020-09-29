ee2867b644add060985c8f4d98dc5e433c8d3aed
Duane Maldonado <duanem33@gmail.com>
2020-09-29 00:42:25 -0400

    updated index. and main.handlebars.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
 ---seeds Burgers into Database
 
 INSERT INTO burgers_db.burgers(burger_name, devoured)
-VALUES("Big Mac", 0),
+VALUES
+  ("Big Mac", 0),
   ("Cheeseburger", 0),
   ("Double Cheeseburger", 0),
-  ("McDoubles", 0),
+  ("McDouble", 0),
   ("Whooper", 0),
-  ("Quarter Pounder", 0),
-  ("Double Double", 0);
+  ("Quarter Pounder", 0);
